7f750ec3f272928ba6f31639374eb62b4a5df928
chenxinxiner <1074537862@qq.com>
2019-10-18 17:50:58 +0800

    BST删除最小键、查找、排名、向上向下取整

--- src/main/java/collection/tree/BinarySearchTree.java
+++ src/main/java/collection/tree/BinarySearchTree.java
@@ -89,10 +89,11 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     private Key min(Node root) {
         if (root.leftNode == null) {
             return root.key;
-        }else{
+        } else {
             return min(root.leftNode);
         }
     }
+
     public Key max() {
         return max(root);
     }
@@ -100,8 +101,156 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     private Key max(Node root) {
         if (root.rightNode == null) {
             return root.key;
-        }else{
-            return min(root.rightNode);
+        } else {
+            return max(root.rightNode);
+        }
+    }
+
+    /**
+     * 向下取整（小于等于key的最大键）
+     *
+     * @Description
+     * @Params [key]
+     * @Return Key
+     * @Author chenxin
+     * @Date 2019-10-18 10:56
+     * @Version 1.0
+     **/
+    public Key floor(Key key) {
+        return floor(root, key);
+    }
+
+    private Key floor(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return floor(node.leftNode, key);
+        } else if (cmp > 0) {
+            Key floorKey = floor(node.rightNode, key);
+            if (floorKey != null) {
+                return floorKey;
+            } else {
+                return node.key;
+            }
+        } else {
+            return node.key;
+        }
+    }
+
+    /**
+     * 向下取整（大于等于key的最小键）
+     *
+     * @Description
+     * @Params [key]
+     * @Return Key
+     * @Author chenxin
+     * @Date 2019-10-18 11:26
+     * @Version 1.0
+     **/
+    public Key ceiling(Key key) {
+        return ceiling(root, key);
+    }
+
+    private Key ceiling(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp > 0) {
+            return ceiling(node.rightNode, key);
+        } else if (cmp < 0) {
+            Key ceilingKey = ceiling(node.leftNode, key);
+            if (ceilingKey != null) {
+                return ceilingKey;
+            } else {
+                return node.key;
+            }
+        } else {
+            return node.key;
+        }
+    }
+
+    /**
+     * 给定位置，返回对应位置的键
+     *
+     * @Description
+     * @Params [k]
+     * @Return Key
+     * @Author chenxin
+     * @Date 2019-10-18 15:03
+     * @Version 1.0
+     **/
+    public Key select(int k) throws Exception {
+        if (k < 0 || k > this.size()) {
+            throw new Exception("范围逸出");
+        }
+        return select(root, k);
+    }
+
+    private Key select(Node node, int k) {
+        int rank = node.n - size(node.rightNode);
+        if (k < rank) {
+            return select(node.leftNode, k);
+        } else if (k > rank) {
+            return select(node.rightNode, k - rank);
+        } else {
+            return node.key;
+        }
+    }
+
+    /**
+     * 给定键，返回在BST中排名
+      *@Description
+      *@Params [key]
+      *@Return int
+      *@Author chenxin
+      *@Date 2019-10-18 15:47
+      *@Version 1.0
+     * rnak(node) = size(left)+node的前面的
+      **/
+    public int rank(Key key) {
+        return rank(root, key,1);
+    }
+
+    private int rank(Node node, Key key,int k) {
+        if (node == null) {
+            return -1;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp > 0) {
+            return rank(node.rightNode, key,k+size(node.leftNode)+1);
+        } else if (cmp < 0) {
+            return rank(node.leftNode, key ,k);
+        } else {
+            return k+size(node.leftNode);
+        }
+    }
+
+    /**
+     * 删除最小键
+      *@Description
+      *@Params []
+      *@Return void
+      *@Author chenxin
+      *@Date 2019-10-18 16:37
+      *@Version 1.0
+      **/
+    public void deleteMin() {
+        root =deleteMin(root);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.leftNode != null) {
+            node.leftNode = deleteMin(node.leftNode);
+            node.n = size(node.leftNode)+size(node.rightNode)+1;
+            return node;
+        } else {
+            return node.rightNode;
         }
     }
 }
--- src/test/java/collection/tree/BSTTest.java
+++ src/test/java/collection/tree/BSTTest.java
@@ -5,32 +5,45 @@ import org.junit.jupiter.api.Test;
 class BSTTest {
 
     @Test
-    void put() {
+    void put() throws Exception {
         BinarySearchTree<String, String> stringIntegerBST = new BinarySearchTree<String, String>();
         stringIntegerBST.put("h","h");
         stringIntegerBST.put("a","a");
-        stringIntegerBST.put("c","asdasda");
         stringIntegerBST.put("f","qweq");
-        stringIntegerBST.put("e","e");
-        stringIntegerBST.put("j","j");
-        stringIntegerBST.put("d","d");
-        stringIntegerBST.put("K","K");
         stringIntegerBST.put("m","m");
+        stringIntegerBST.put("e","e");
+        stringIntegerBST.put("c","asdasda");
+        stringIntegerBST.put("d","d");
+        stringIntegerBST.put("j","j");
+        stringIntegerBST.put("k","k");
         System.out.println(stringIntegerBST.size()+" "+stringIntegerBST.get("c"));
-        System.out.println(stringIntegerBST.min());
-        System.out.println(stringIntegerBST.max());
+        System.out.println("max= "+stringIntegerBST.min());
+        System.out.println("min= "+stringIntegerBST.max());
+        System.out.println("floor= "+stringIntegerBST.floor("g"));
+        System.out.println("ceiling= "+stringIntegerBST.ceiling("g"));
+        System.out.println("select= "+stringIntegerBST.select(4));
+        System.out.println("rank= "+stringIntegerBST.rank("j"));
+        stringIntegerBST.deleteMin();
+        System.out.println();
     }
 
     @Test
-    void get() {
-        int i = 1;
-        switch (i) {
-            case 1:
-                System.out.printf("1");
-            case 2:
-                System.out.printf("2");
-            case 3:
-                System.out.printf("3");
+    void get() throws Exception {
+        BinarySearchTree<Double,Double> binarySearchTree = new BinarySearchTree();
+        double[] doubles = new double[10000000];
+        for (int i = 0; i < 10000000; i++) {
+            double n = Math.random();
+            binarySearchTree.put(n,n);
+            doubles[i] = n;
         }
+        System.out.println();
+        long l = System.currentTimeMillis();
+        System.out.println(binarySearchTree.get(doubles[399909]));
+        System.out.println(binarySearchTree.get(doubles[120000]));
+        System.out.println("floor= "+binarySearchTree.floor(0.12312314134));
+        System.out.println("ceiling= "+binarySearchTree.ceiling(0.123123141434));
+        System.out.println("select= "+binarySearchTree.select(1321233));
+        System.out.println("rank= "+binarySearchTree.rank(doubles[231232]));
+        System.out.println("时间 ="+(System.currentTimeMillis()-l));
     }
 }
